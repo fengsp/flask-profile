d291b06d4b678fe6b7c97c7c8311071e1d1d7634
fsp <fsp261@gmail.com>
2014-05-12 17:17:39 +0800

    Added Profiler

--- flask_profile/__init__.py
+++ flask_profile/__init__.py
@@ -3,8 +3,27 @@
     flaskext.profile
     ~~~~~~~~~~~~~~~~
 
-    Description of the module goes here...
+    Adds profiling to Flask.
 
     :copyright: (c) 2014 by Shipeng Feng.
     :license: BSD, see LICENSE for more details.
 """
+
+import profile
+
+
+class Profiler(object):
+    """Profiler.
+    """
+    
+    def __init__(self, app=None):
+        self.app = app
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        """This is used to set up profiler for your flask app object.
+
+        :param app: the Flask app object.
+        """
+        pass
